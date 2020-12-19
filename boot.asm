a0a32524b201106f9d9c57b4a37ebf806a1d56a6
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2020-12-19 15:47:44 +0530

    final code with comments

--- boot.asm
+++ boot.asm
@@ -1,18 +1,21 @@
+; Name: Ananya Lohani
+; Roll Number: 2019018
+
+; starting in real mode (16 bit)
 bits 16
 org 0x7c00
 
 boot:
-    mov ax, 0x2401
-    int 0x15
     mov ax, 0x3
-    int 0x10
-    cli
-    lgdt [globdesc_ptr]
-    mov eax, cr0
+    int 0x10                ; set VGA text mode 3
+    cli                     ; disable interrupts
+    lgdt [globdesc_ptr]     ; load the global descriptor table
+    mov eax, cr0            ; set protected mode bit in cr0
     or eax, 0x1
     mov cr0, eax
-    jmp CODE:main_code
+    jmp CODE:main_code      ; jump to CODE segment
 
+; setting up the global descriptor table
 globdesc_start:
     dq 0x0
 
@@ -32,44 +35,38 @@ globdesc_code:
     db 11001111b
     db 0x0
 
-globdesc_end:
+CODE equ globdesc_code - globdesc_start
 
+globdesc_end:
 globdesc_ptr:
     dw globdesc_end - globdesc_start
     dd globdesc_start
 
-CODE equ globdesc_code - globdesc_start
-DATA equ globdesc_data - globdesc_start
-
+; shifting to protected mode (32-bit)
 bits 32
 
 main_code:
-    mov ax, DATA
-    mov gs, ax
-    mov es, ax
-    mov ss, ax
-    mov ds, ax
-    mov fs, ax
-    mov esi, msg
-    mov ebx, 0xb8000
+    mov esi, msg                ; store the message string in esi
+    mov ebx, 0xb8000            ; initialising the VGA buffer
 
 .loop:
-    lodsb
-    or al, al
-    jz stop
-    or eax, 0x0F00
-    mov word [ebx], ax
-    add ebx, 2
-    jmp .loop
+    lodsb                       ; load esi to al and increments esi
+    or al, al                   ; checks if end of string has been reached
+    jz stop                     ; jump to stop if al is zero
+    or eax, 0x0F00              ; set text color to white
+    mov word [ebx], ax          ; move a single character of the message to the buffer
+    add ebx, 2                  ; increment buffer pointer
+    jmp .loop                   ; loop again
 
 stop:
-    mov eax, cr0
+    mov eax, cr0                ; to print the value of cr0
     or eax, 0x0F00
     mov [ebx], eax
-    cli
-    hlt
+    cli                         ; disable al interrupts
+    hlt                         ; halt execution
 
-msg: db "Hello, World. ", 0
+msg: db "Hello, World. Value of cr0 in ASCII:", 0
 
+; Magic numbers for loading the binary as a bootable image
 times 510 - ($ - $$) db 0
 dw 0xaa55
